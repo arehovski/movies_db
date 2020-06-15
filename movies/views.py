92a6d7bdd0621a471f7df023e880de161eb19bb1
arehovski <sergei_arehovski@mail.ru>
2020-06-15 21:58:34 +0300

    Minor changes made

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django import views
+from .models import Actor, Director, Movie
+
 
 # Create your views here.
+class HomePage(views.View):
+    def get(self, request):
+        actors = Actor.objects.all()
+        directors = Director.objects.all()
+        movies = Movie.objects.all()
+        context = {
+            'actors': actors,
+            'directors': directors,
+            'movies': movies
+        }
+        return render(request, 'base.html', context)
