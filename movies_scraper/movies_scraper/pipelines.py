7f7d52f33c38acc06f66bbeb81cd0cb36dc5ba25
arehovski <sergei_arehovski@mail.ru>
2020-06-18 22:44:14 +0300

    Created abstract model for actors and directors. Debuging spider

--- movies_scraper/movies_scraper/pipelines.py
+++ movies_scraper/movies_scraper/pipelines.py
@@ -5,8 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+from movies.models import Director, Actor, Genre, Movie
+from .items import DirectorItem, ActorItem, GenreItem, MovieItem
+import logging
+
 
 class MoviesScraperPipeline:
     def process_item(self, item, spider):
-        item_what = item
         return item
