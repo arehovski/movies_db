6ab9ab7fd01ed1421858a0120b59c148411feb16
arehovski <sergei_arehovski@mail.ru>
2020-06-27 11:25:03 +0300

    Added director and actor views. Done code refractoring in genre, country and year views.

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import HomePage, GenreView, YearView, CountryView, MovieView
+from .views import HomePage, GenreView, YearView, CountryView, MovieView, DirectorView, ActorView
 
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home_page'),
-    path('genres/<genre>', GenreView.as_view(), name='genre_view'),
-    path('year/<year>', YearView.as_view(), name='year_view'),
-    path('country/<country>', CountryView.as_view(), name='country_view'),
+    path('genres/<param>', GenreView.as_view(), name='genre_view'),
+    path('year/<param>', YearView.as_view(), name='year_view'),
+    path('country/<param>', CountryView.as_view(), name='country_view'),
     path('movie/<int:pk>', MovieView.as_view(), name='movie_view'),
+    path('director/<int:pk>', DirectorView.as_view(), name='director_view'),
+    path('actor/<int:pk>', ActorView.as_view(), name='actor_view'),
 
 ]
