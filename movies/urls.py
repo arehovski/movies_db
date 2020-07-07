550e86c2a4aaa4d90e2e8ea3b5f9d8d69faa2928
arehovski <sergei_arehovski@mail.ru>
2020-07-07 21:45:59 +0300

    Fully added wishlist feature

--- movies/urls.py
+++ movies/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('logout', logout_view, name='logout'),
     path('search', SearchView.as_view(), name='search'),
     path('add_movie/<int:pk>', add_to_wish_list, name='add_movie'),
+    path('rm_movie/<int:pk>', remove_from_wish_list, name='rm_movie'),
     path('wish_list', WishListView.as_view(), name='wish_list')
 ]
